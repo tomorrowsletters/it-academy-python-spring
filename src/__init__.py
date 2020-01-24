985a37305f72339a3104c7e99d77ba14eae662ab
tomorrowsletters <surilogw@gmail.com>
2020-01-25 01:32:00 +0300

    Fix multiline comment

--- src/__init__.py
+++ src/__init__.py
@@ -140,8 +140,7 @@ if __name__ == '__main__':
 def median2(pool):
     """Statistical median to demonstrate doctest.
     >>> median2([2, 9, 9, 7, 9, 2, 4, 5, 8])
-    6 #change to 7 in order to pass the test
-    """
+    6 #change to 7 in order to pass the test"""
     copy = sorted(pool)
     size = len(copy)
     if size % 2 == 1:

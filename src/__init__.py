c3c3e5114952762462a18649c50f62763adc74a7
tomorrowsletters <surilogw@gmail.com>
2020-01-25 02:29:33 +0300

    Fix arrows

--- src/__init__.py
+++ src/__init__.py
@@ -139,8 +139,9 @@ if __name__ == '__main__':
 
 def median2(pool):
     """Statistical median to demonstrate doctest.
-    >>> median2([2, 9, 9, 7, 9, 2, 4, 5, 8])
-    6 #change to 7 in order to pass the test"""
+    median2([2, 9, 9, 7, 9, 2, 4, 5, 8])
+    6 #change to 7 in order to pass the test
+    """
     copy = sorted(pool)
     size = len(copy)
     if size % 2 == 1:

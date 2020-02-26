a3395084a2be0797996ba49c306ba9a8ad7cf544
Kiryl Karotki <surilogw@gmail.com>
2020-02-26 22:18:55 +0300

    Refactor multiline comment block

--- src/correct_senten.py
+++ src/correct_senten.py
@@ -1,7 +1,7 @@
 def correct_sentence(text: str) -> str:
-    """ returns a corrected sentence which starts with a capital letter
-    and ends with a dot.
+    """returns a corrected sentence which starts with a capital letter
 
+    and ends with a dot.
     """
     text = text[0].upper() + text[1:]
     if (text[-1] != '.'):
